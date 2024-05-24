ddc463a0f27ef197e6a2c060c5ef8cd53fb4e05b
alexandr antsiferov <sania.antsiferoff@yandex.ru>
2024-05-24 02:46:31 +0200

    NEW MAIN

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config import OPERATION_PATH, OPERATIONS_COUNT
-from src.utils import get_operations_instances, sort_operations, load_date, get_operations
+from src.utils import get_operations_instances, sort_operations, load_date, get_operations_executed
 
 
 def main():
@@ -7,11 +7,11 @@ def main():
     Основная функция
     """
     operations = load_date(OPERATION_PATH)
-    get_operation = get_operations(operations)
-    operations = get_operation[:OPERATIONS_COUNT]
-    operation_instance = get_operations_instances(operations)
+    operations_executed = get_operations_executed(operations)
+    operation_instance = get_operations_instances(operations_executed)
     sorted_operations = sort_operations(operation_instance)
-    for operation in sorted_operations:
+    selected_operations = sorted_operations[:OPERATIONS_COUNT]
+    for operation in selected_operations:
         print()
         print(operation)
 
